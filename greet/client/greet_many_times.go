4b26073890bc23b819b6431821e49aa2d6698c14
codeBuddha <157468650+jamwujustyle@users.noreply.github.com>
2026-04-16 08:19:42 +0000

    sync

--- greet/client/greet_many_times.go
+++ greet/client/greet_many_times.go
@@ -1,17 +1,34 @@
-import (
-	"fmt"
-	pb "github.com/jamwujustyle/gogrpc/greet/proto"
-	"context"
-)
+package main
 
-func (c pb.GreetClientService)  {
-	fmt.Println("GreetManyTimes invoked") 
+import (
+	pb "github.com/jamwujustyle/gogrpc/greet/proto" 
+	"log"
+	"context"
+	"io"
+	"fmt"
+	)
+
+func doGreetManyTimes(c pb.GreetServiceClient) {
+	log.Println("GreetManyTimes invoked")
 
 	req:= &pb.GreetRequest{
-		FirstName: "wind",
+		FirstName: "Zhamshid",
 	}
-	stream, err:= c.GreetMa
-	for {
 
+	stream, err:= c.GreetManyTimes(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes: \v\n", err)
 	}
+
+	for {
+		msg, err:= stream.Recv()
+		
+		if err == io.EOF {
+			break
+		}
+		
+		fmt.Printf("GreetManyTimes: %s\n", msg.Result)
+	}
+
 }
--- greet/server/greet_many_times.go
+++ greet/server/greet_many_times.go
@@ -4,13 +4,18 @@ import (
 	pb "github.com/jamwujustyle/gogrpc/greet/proto" 
 )
 
-func (s *Server) (in *pb.GreetRequest, stream pb.GreetService_GreetManyTimesServer) error {
+func (s *Server) GreetManyTimes(in *pb.GreetRequest, stream pb.GreetService_GreetManyTimesServer) error {
 	fmt.Printf("greetmanytimes invoked with %v", in)
 
 
 	for i:= 0; i < 10; i++ {
 		res:= fmt.Sprintf("greeted %s %d many tiems", in.FirstName, i)
+	
+		stream.Send(&pb.GreetResponse{
+		Result: res,
+		})
 	}
+
 	return nil
 }
 
