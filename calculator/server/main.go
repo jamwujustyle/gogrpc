e574edcc0dded9ef26fbf0b72ce1aa4135000f06
codeBuddha <jamwujustyle@gmail.com>
2026-04-19 15:28:01 +0500

    checkpoint

--- calculator/server/main.go
+++ calculator/server/main.go
@@ -7,26 +7,26 @@ import (
 
 	pb "github.com/jamwujustyle/gogrpc/calculator/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 var addr string = "0.0.0.0:50051"
-
-
 
 type Server struct {
 	pb.UnimplementedCalculatorServiceServer
 }
 
 func main() {
-	lis, err:= net.Listen("tcp", addr)
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to establish connection: %v\n", err)
 	}
 	fmt.Printf("listening on: %s\n", addr)
 
-	s:= grpc.NewServer()
+	s := grpc.NewServer()
 
 	pb.RegisterCalculatorServiceServer(s, &Server{})
+	reflection.Register(s)
 
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("Failed to listen: %v\n", err)
