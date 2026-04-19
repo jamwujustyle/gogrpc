314f389869b4ea27fff8e2782bb06decf9178a58
codeBuddha <jamwujustyle@gmail.com>
2026-04-19 15:15:24 +0500

    tls

--- greet/client/main.go
+++ greet/client/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"log"
-	"time"
+	"log/slog"
 
 	pb "github.com/jamwujustyle/gogrpc/greet/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true // can change that to false if needed
+	opts := []grpc.DialOption{}
+
+	if tls {
+		cF := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(cF, "")
+
+		if err != nil {
+			slog.Info("error while loading CA trust cert", "err", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+
+	}
+	conn, err := grpc.NewClient(addr, opts...)
 
 	if err != nil {
 		log.Fatalf("failed to establish connection %v\n", err)
@@ -22,11 +36,11 @@ func main() {
 
 	c := pb.NewGreetServiceClient(conn)
 
-	// doGreet(c)
+	doGreet(c)
 
 	// doGreetManyTimes(c)
 
 	// doLongGreet(c)
 	// doGreetEveryone(c)
-	doGreetWithDeadline(c, 2*time.Second)
+	// doGreetWithDeadline(c, 2*time.Second)
 }
--- greet/server/main.go
+++ greet/server/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"net"
+	"os"
 
 	pb "github.com/jamwujustyle/gogrpc/greet/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "0.0.0.0:50051"
@@ -15,7 +18,7 @@ type Server struct {
 }
 
 func main() {
-	lis, err:= net.Listen("tcp", addr)
+	lis, err := net.Listen("tcp", addr)
 
 	if err != nil {
 		log.Fatalf("failed to listen  on: %v\n", err)
@@ -23,7 +26,24 @@ func main() {
 
 	log.Printf("listening on %s\n", addr)
 
-	s:= grpc.NewServer()
+	opts := []grpc.ServerOption{}
+	tls := true // change that to false if needed
+
+	if tls {
+		cF := "ssl/server.crt"
+		kF := "ssl/server.pem"
+
+		creds, err := credentials.NewServerTLSFromFile(cF, kF)
+
+		if err != nil {
+			slog.Error("Failed loading certs", "err", err)
+			os.Exit(1)
+		}
+
+		opts = append(opts, grpc.Creds(creds))
+	}
+
+	s := grpc.NewServer(opts...)
 
 	pb.RegisterGreetServiceServer(s, &Server{})
 
